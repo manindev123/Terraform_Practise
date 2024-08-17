ee0e424393f6653c0fe0b1ac38e11208179eaf3b
manindev123 <manindev123@gmail.com>
2024-08-17 17:12:05 +0530

    trfm

---
+++ Ec2/Ec2.tf
@@ -0,0 +1,15 @@
+# Configure the AWS Provider
+provider "aws" {
+  version = "~> 5.0"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "web"{
+   ami          = ami-0ae8f15ae66fe8cda
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+
+}
